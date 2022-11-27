266713bce3763d6f563e233ab9025105535dae92
Anne Ananwune <94768814+Annahbellah@users.noreply.github.com>
2022-11-27 07:32:49 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ====================== typing animation ==================== */
+var typed = new Typed(".typing",{
+    strings:["","Software Engineer","Database Design Expert","Full Stack Developer","Workflow Automation Expert"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+});
